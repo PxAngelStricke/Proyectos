67795248252b4399575657cbede2ea7e658d0be3
PxAngelStricke <pequis108@gmail.com>
2024-03-05 20:33:21 -0600

    Angular 16 y 17 creando proyectos

---
+++ Angular/curso-angular16/src/app/padre/padre.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-padre',
+  templateUrl: './padre.component.html',
+  styleUrls: ['./padre.component.css']
+})
+export class PadreComponent {
+  mensajeRecibido: string = '';
+
+  recibirMensaje($event: string) {
+    this.mensajeRecibido = $event;
+  }
+
+  // CONTADOR
+  valorContador: number = 0;
+
+  incrementar() {
+    this.valorContador++;
+  }
+
+  decrementar() {
+    this.valorContador--;
+  }
+}
